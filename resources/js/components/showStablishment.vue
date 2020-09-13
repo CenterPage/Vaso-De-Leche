7c2f7f6735647d8b265ffea8655d1c08f0943635
Eddy Rufino <eddyjaair@gmail.com>
2020-09-13 10:40:47 -0500

    add[vue2-leaflet] and leaflet[npm] and show mapa in each stablishment

--- resources/js/components/showStablishment.vue
+++ resources/js/components/showStablishment.vue
@@ -6,47 +6,54 @@
 				<img :src="`../storage/${stablishment.image}`" alt="" style="width: 400px;">
 				<p class="mt-5">{{ stablishment.description }}</p>
 			</div>
-			<aside class="col-md-4 bg-primary">
-				<div class=""></div>
-				<div class="text-center text-white mt-2 mb-4">
-					Más información
+			<aside class="col-md-4 ">
+				<div class="">
+					<mapa-location></mapa-location>
 				</div>
-				<p class="text-white mt-1">
-					<span class="font-weight-bold">Ubicación</span>
-					{{ stablishment.address }}
-				</p>
-				<p class="text-white mt-1">
-					<span class="font-weight-bold">Horario</span>
-					{{ stablishment.open }} - {{ stablishment.close }}
-				</p>
-				<p class="text-white mt-1">
-					<span class="font-weight-bold">Telefono</span>
-					{{ stablishment.phone }}
-				</p>
+				<div class="text-center text-white mt-2 mb-4 bg-primary">
+					<h2>Más información</h2>
+					<p class="text-white mt-1">
+						<span class="font-weight-bold">Ubicación</span>
+						{{ stablishment.address }}
+					</p>
+					<p class="text-white mt-1">
+						<span class="font-weight-bold">Horario</span>
+						{{ stablishment.open }} - {{ stablishment.close }}
+					</p>
+					<p class="text-white mt-1">
+						<span class="font-weight-bold">Telefono</span>
+						{{ stablishment.phone }}
+					</p>
+				</div>
+
 			</aside>
 		</div>
 	</div>
 </template>
 
 <script>
-	export default {
-		methods: {
-			getStablishment() {
-				const { id } = this.$route.params;
-				// No olvides poner el "/" antes de cualquier URL porque sino coge todo el nombre de la otra URL
-				axios.get('/api/stablishments/' + id)
-					.then(response => {
-						this.$store.commit('GET_STABLISHMENT', response.data);
-					})
-			}
-		},
-		mounted() {
-			this.getStablishment();
-		},
-		computed: {
-			stablishment() {
-				return this.$store.getters.getStablishment;
-			}
+import mapaLocation from './mapaLocation';
+export default {
+	components: {
+		mapaLocation
+	},
+	methods: {
+		getStablishment() {
+			const { id } = this.$route.params;
+			// No olvides poner el "/" antes de cualquier URL porque sino coge todo el nombre de la otra URL
+			axios.get('/api/stablishments/' + id)
+				.then(response => {
+					this.$store.commit('GET_STABLISHMENT', response.data);
+				})
+		}
+	},
+	mounted() {
+		this.getStablishment();
+	},
+	computed: {
+		stablishment() {
+			return this.$store.getters.getStablishment;
 		}
 	}
+}
 </script>
